194aca1e9952b6ac2afd4ec5306d924d0beb3e02
aniketsahu115 <sahuaniket115@gmail.com>
2025-04-09 04:11:33 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  const frontendPath = path.join(process.cwd(), "frontend", "dist");
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
